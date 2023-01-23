af069f723123913c3c845115f78f61423aef6cb5
DESKTOP-OI19NCO\ppyum <doeon93@naver.com>
2023-01-23 16:46:27 +0900

    fix : hilt AndroidEntrypoint 누락 추가 및 internet 퍼미션 추가

--- app/src/main/java/com/example/cleanarchitecturestudy/presenter/MainActivity.kt
+++ app/src/main/java/com/example/cleanarchitecturestudy/presenter/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.cleanarchitecturestudy.presenter
 import com.example.cleanarchitecturestudy.BaseActivity
 import com.example.cleanarchitecturestudy.R
 import com.example.cleanarchitecturestudy.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override val layoutId: Int
